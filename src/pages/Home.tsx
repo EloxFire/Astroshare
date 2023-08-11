9d4ffa08b3e075ff90215088322722b5faa35d48
Enzo Avagliano <eavagliano@laprovence.com>
2023-08-11 23:35:49 +0200

    WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,11 @@ import '../styles/pages/home.scss'
 
 export default function Home() {
   return (
-    <div id="home">Home</div>
+    <div id="home">
+      <div className="home__left">
+
+      </div>
+      <div className="home__right"></div>
+    </div>
   )
 }
