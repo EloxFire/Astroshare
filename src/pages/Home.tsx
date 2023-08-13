3eb5bbfdab52b15b36dd32a2cc5608d208627caa
Enzo Avagliano <eavagliano@laprovence.com>
2023-08-13 16:17:01 +0200

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,17 @@ import '../styles/pages/home.scss'
 
 export default function Home() {
   return (
-    <div id="home">
+    <div className="home">
       <div className="home__left">
-
+        <h1 className="h1 title">L'astronomie, simplement.</h1>
+        <div className="subtitles-container">
+          <p>Explorez les splendeurs infinies de l'univers avec simplicité grâce à notre guide d'astronomie pour débutants.</p>
+          <p>Apprenez à maitriser votre matériel, complétez le catalogue de Messier, faites vos premiers pas en Astrophotographie.</p>
+        </div>
       </div>
-      <div className="home__right"></div>
+      <div className="home__right">
+        <img src="/images/mars.png" alt="Planète mars" />
+      </div>
     </div>
   )
 }
