74a09ca93675fbf5eec36bbd2729e4d7c6ac4db7
Enzo Avagliano <eavagliano@laprovence.com>
2023-08-23 22:16:09 +0200

    WIP on navbar responsive (dropdown)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,9 @@ import { routes } from '../routes'
 import '../styles/components/navbar.scss'
 
 export default function Navbar() {
+
+
+
   return (
     <div className="navbar">
       <div className="navbar__logo">
@@ -16,6 +19,14 @@ export default function Navbar() {
       </div>
       <div className="navbar__cta">
         <Link className="button-link" to={routes.contact.path}>{routes.contact.label}</Link>
+        <div className="dropdown">
+          <button className="dropbtn">Dropdown</button>
+          <div className="dropdown-content">
+            <Link className="link" to={routes.home.path}>{routes.home.label}</Link>
+            <Link className="link" to={routes.about.path}>{routes.about.label}</Link>
+            <Link className="link" to={routes.gallery.path}>{routes.gallery.label}</Link>
+          </div>
+        </div>
       </div>
     </div>
   )
