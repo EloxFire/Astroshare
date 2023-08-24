b53135aaaef3b7c35b8391a60393d2045c6548ae
Enzo Avagliano <eavagliano@laprovence.com>
2023-08-24 09:12:29 +0200

    Removed about route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import RessourceDetails from "./pages/RessourceDetails";
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path={routes.home.path} element={<Layout component={<Home />} />} />
-        <Route path={routes.about.path} element={<Layout component={<About />} />} />
+        {/* <Route path={routes.about.path} element={<Layout component={<About />} />} /> */}
         <Route path={routes.contact.path} element={<Layout component={<Contact />} />} />
         <Route path={routes.ressources.path} element={<Layout component={<RessourceDetails />} />} />
         <Route path={routes.notFound.path} element={<NotFound />} />
