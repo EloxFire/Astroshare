4c28482dd81560978ce715242cd5cf6c50974b95
Enzo Avagliano <eavagliano@laprovence.com>
2023-08-28 00:00:40 +0200

    Added images infos display

--- src/components/CustomImageDisplay.tsx
+++ src/components/CustomImageDisplay.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 import '../styles/components/customImageDisplay.scss'
+import { Image } from '../scripts/types'
+import dayjs from 'dayjs'
 
 interface CustomImageDisplayProps {
-  slides: { url: string, alt: string }[]
+  slides: Image[]
 }
 
 export default function CustomImageDisplay({ slides }: CustomImageDisplayProps) {
 
   const [slideNumber, setSlideNumber] = useState(0)
-  const [openModal, setOpenModal] = useState(true)
+  const [openModal, setOpenModal] = useState(false)
 
   return (
     <div className="custom-image-display">
@@ -34,8 +36,33 @@ export default function CustomImageDisplay({ slides }: CustomImageDisplayProps) 
           <div className="custom-image-display__modal__header">
             <button className="custom-image-display__modal__header__close-button" onClick={() => { setOpenModal(false) }}>&#9747;</button>
           </div>
-          <div className="custom-image-display__modal__image-container">
-            <img src={slides[slideNumber].url} alt={slides[slideNumber].alt} />
+          <div className="custom-image-display__modal__content">
+            <div className="custom-image-display__modal__content__image-container">
+              <img src={slides[slideNumber].url} alt={slides[slideNumber].alt} />
+            </div>
+            <div className="custom-image-display__modal__content__image-infos">
+              <small className="top-element">Date : {dayjs(slides[slideNumber].date).format('DD/MM/YYYY')}</small>
+              {slides[slideNumber].resolution && <small className="top-element">Resolution : {slides[slideNumber].resolution}</small>}
+              {slides[slideNumber].fileFormat && <small className="top-element">Format : {slides[slideNumber].fileFormat}</small>}
+              {slides[slideNumber].cameraSettings && <small className="top-element">Camera :</small>}
+              {slides[slideNumber].cameraSettings?.name && <small className="child-element">Nom : {slides[slideNumber].cameraSettings?.name}</small>}
+              {slides[slideNumber].cameraSettings?.iso && <small className="child-element">ISO : {slides[slideNumber].cameraSettings?.iso}</small>}
+              {slides[slideNumber].cameraSettings?.shutter && <small className="child-element">Vitesse d'obturation : {slides[slideNumber].cameraSettings?.shutter}</small>}
+              {slides[slideNumber].cameraSettings?.aperture && <small className="child-element">Ouverture : {slides[slideNumber].cameraSettings?.aperture}</small>}
+              {slides[slideNumber].cameraSettings?.focalLength && <small className="child-element">Focale : {slides[slideNumber].cameraSettings?.focalLength}</small>}
+              {slides[slideNumber].cameraSettings?.fps && <small className="child-element">FPS : {slides[slideNumber].cameraSettings?.fps}</small>}
+              {slides[slideNumber].scopeSettings && <small className="top-element">Telescope :</small>}
+              {slides[slideNumber].scopeSettings?.name && <small className="child-element">Nom : {slides[slideNumber].scopeSettings?.name}</small>}
+              {slides[slideNumber].scopeSettings?.diameter && <small className="child-element">Diametre : {slides[slideNumber].scopeSettings?.diameter}</small>}
+              {slides[slideNumber].scopeSettings?.focal && <small className="child-element">Focale : {slides[slideNumber].scopeSettings?.focal}</small>}
+              {slides[slideNumber].scopeSettings?.focalRatio && <small className="child-element">Rapport F/D : {slides[slideNumber].scopeSettings?.focalRatio}</small>}
+              {slides[slideNumber].scopeSettings?.mount && <small className="child-element">Monture : {slides[slideNumber].scopeSettings?.mount}</small>}
+              {slides[slideNumber].scopeSettings?.eyePiece && <small className="child-element">Occulaire : {slides[slideNumber].scopeSettings?.eyePiece}</small>}
+              {slides[slideNumber].scopeSettings?.eyePieceMagnification && <small className="child-element">Grossisement : {slides[slideNumber].scopeSettings?.eyePieceMagnification}x</small>}
+              {slides[slideNumber].scopeSettings?.barlow && <small className="child-element">Barlow : {slides[slideNumber].scopeSettings?.barlow}</small>}
+              {slides[slideNumber].processingSettings && <small className="top-element">Post-traitement :</small>}
+              {slides[slideNumber].processingSettings?.software && <small className="child-element">Logiciel : {slides[slideNumber].processingSettings?.software}</small>}
+            </div>
           </div>
           <div className="custom-image-display__modal__footer">
             <button className="custom-image-display__modal__footer__swipe-button" onClick={() => { setSlideNumber(slideNumber - 1) }} disabled={slideNumber === 0}>&#10094;</button>
@@ -44,6 +71,6 @@ export default function CustomImageDisplay({ slides }: CustomImageDisplayProps) 
           </div>
         </div>
       }
-    </div>
+    </div >
   )
 }
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -5,12 +5,20 @@ import { images } from '../scripts/helpers/gallery'
 
 export default function Gallery() {
   return (
-    <div className="gallery">
-      <h1>Gallerie photos</h1>
-      <p>Retrouvez une sélection de mes photos favorites, lors de mes différentes sessions d'astronomie.</p>
-      <CustomImageDisplay
-        slides={images}
-      />
-    </div>
+    <>
+      <div className="gallery">
+        <h1>Gallerie photos</h1>
+        <p>Retrouvez une sélection de mes photos favorites, lors de mes différentes sessions d'astronomie.</p>
+        <CustomImageDisplay
+          slides={images}
+        />
+      </div>
+      <div className="gallery-mobile">
+        <h1>Gallerie photos</h1>
+        <p>Afin de garantir une bonne experience, la gallerie photo n'est disponible que sur PC.</p>
+        <p>Merci de votre comprehension.</p>
+        <a className="link" href="/">Retour à l'accueil</a>
+      </div>
+    </>
   )
 }
