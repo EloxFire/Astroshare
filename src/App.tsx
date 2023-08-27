b36d85612c6c41e7f862a4e9e6b2ef2f61da2837
Enzo Avagliano <eavagliano6@gmail.com>
2023-08-27 22:49:57 +0200

    Update App.tsx
    Fixed about page import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
-// import About from "./pages/About";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import RessourceDetails from "./pages/RessourceDetails";
