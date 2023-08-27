c43cef3e084a57d323590f927bb6e8ca5932c252
Enzo Avagliano <eavagliano6@gmail.com>
2023-08-27 22:48:44 +0200

    Update App.tsx
    Enabled about page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path={routes.home.path} element={<Layout component={<Home />} />} />
-        {/* <Route path={routes.about.path} element={<Layout component={<About />} />} /> */}
+        <Route path={routes.about.path} element={<Layout component={<About />} />} />
         <Route path={routes.contact.path} element={<Layout component={<Contact />} />} />
         <Route path={routes.gallery.path} element={<Layout component={<Gallery />} />} />
         <Route path={routes.ressources.path} element={<Layout component={<RessourceDetails />} />} />
