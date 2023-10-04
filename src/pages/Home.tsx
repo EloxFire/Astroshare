be218a41ffa3d8e1a31192af5cda8a9a10b10d5e
Enzo Avagliano <eavagliano@laprovence.com>
2023-10-04 22:02:47 +0200

    Animated 3D model of mars added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom'
 import { constellations_cheat_sheets } from '../scripts/helpers/ressources/general/constellation_cheat_sheets'
 import { optical_notion } from '../scripts/helpers/ressources/material/optical_notions'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei';
+import MarsModel from '../components/MarsModel';
+import { useRef } from 'react';
+import { useAspectRatio } from '../hooks/useAspectRatio';
 import '../styles/pages/home.scss'
 
 export default function Home() {
+  const containerRef = useRef(null);
+  const aspectRatio = useAspectRatio(containerRef);
+  const scale = [aspectRatio / 100, aspectRatio / 100, aspectRatio / 100];
+
   return (
     <div className="home">
       <div className="home__left">
@@ -20,9 +29,12 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="home__right">
-        <img src="/images/mars.png" alt="Planète mars" />
-        {/* <div className="sketchfab-embed-wrapper"> <iframe title="Planet Mars" frameBorder="0" allowFullScreen allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1a2862763b1c428ba914691248ddb8fa/embed?autostart=1"> </iframe> </div> */}
+      <div ref={containerRef} className="home__right">
+        <Canvas>
+          <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={75} near={0.1} far={1000} />
+          <directionalLight position={[8, 5, 5]} intensity={3} />
+          <MarsModel scale={scale} />
+        </Canvas>
       </div>
     </div>
   )
