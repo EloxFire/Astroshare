eba7b06ad50655f1530d0779955db91834281634
Enzo Avagliano <eavagliano@laprovence.com>
2023-12-11 12:47:19 +0100

    Minor changes on loaders in submit buttons

--- src/pages/dashboard/AddImage.tsx
+++ src/pages/dashboard/AddImage.tsx
@@ -15,7 +15,7 @@ export default function AddImage() {
   }, [])
 
   const [imageTitle, setImageTitle] = useState<string>("")
-  const [imageDate, setImageDate] = useState<string>(dayjs().format("DD-MM-YYYY"))
+  const [imageDate, setImageDate] = useState<string>(dayjs().format("MM-DD-YYYY"))
   const [imageFile, setImageFile] = useState<any>(null);
   const [additionnalProperty, setAdditionnalProperty] = useState<string>("")
   const [additionnalPropertyValue, setAdditionnalPropertyValue] = useState<string>("")
@@ -32,7 +32,7 @@ export default function AddImage() {
       file: imageFile,
       alt: imageTitle,
       date: imageDate,
-      uploadedAt: dayjs(),
+      uploadedAt: new Date(),
       ...optionnalProperties
     }
 
@@ -92,7 +92,7 @@ export default function AddImage() {
             </select>
             <div className="property-input-container">
               <input type='text' className="custom-input" style={{ marginBottom: '0px' }} placeholder="Valeur" value={additionnalPropertyValue} onChange={(e) => { setAdditionnalPropertyValue(e.target.value) }} />
-              <button disabled={uploading} onClick={() => appendNewProperty(additionnalProperty, additionnalPropertyValue)}>Ajouter la propriété</button>
+              <button disabled={uploading} onClick={() => appendNewProperty(additionnalProperty, additionnalPropertyValue)}>{!uploading ? "Ajouter la propriété" : <div className="loader"></div>}</button>
             </div>
             <div className="added-properties">
               <small>Propriétés supplémentaires : <br /></small>
@@ -112,38 +112,6 @@ export default function AddImage() {
             {imageFile && <img className="image" src={URL.createObjectURL(imageFile)} alt='' />}
           </label>
         </div>
-        {/* <div className="add-ressource-details__content__left">
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Titre de l'image" value={imageTitle} onChange={(e) => { setImageTitle(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Date de l'image (DD-MM-YYYY)" onChange={(e) => { setImageDate(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Resolution (1920x1080)" onChange={(e) => { setImageResolution(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Format (png, pdf, etc...)" onChange={(e) => { setImageFormat(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Tags (séparés d'une virgule sans espaces)" onChange={(e) => { setImageTags(e.target.value.split(',')) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Nom de la caméra" onChange={(e) => { setImageCameraName(e.target.value) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="ISO" onChange={(e) => { setImageCameraIso(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Shutter speed" onChange={(e) => { setImageCameraShutterSpeed(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Aperture" onChange={(e) => { setImageCameraAperture(e.target.value) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="FPS" onChange={(e) => { setImageCameraFps(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Focale" onChange={(e) => { setImageCameraFocal(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Télescope" onChange={(e) => { setImageTelescopeName(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Monture" onChange={(e) => { setImageTelescopeMount(e.target.value) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Focale" onChange={(e) => { setImageTelescopeFocal(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Diametre" onChange={(e) => { setImageTelescopeDiameter(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="F/d" onChange={(e) => { setImageTelescopeFocalRatio(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Oculaire" onChange={(e) => { setImageTelescopeEyepiece(e.target.value) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Grossissement" onChange={(e) => { setImageTelescopeMagnification(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Barlow ?" onChange={(e) => { setImageTelescopeBarlow(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Logiciel de traitement" onChange={(e) => { setImageSoftware(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Stacking ?" onChange={(e) => { setImageStacking(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Logiciel de stacking" onChange={(e) => { setImageStackingSoftware(e.target.value) }} />
-          <input type='number' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Nombre d'images stackées" onChange={(e) => { setImageStackingImages(e.target.value.includes('.') ? parseFloat(e.target.value) : parseInt(e.target.value)) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Pose / image" onChange={(e) => { setImageExposurePerImage(e.target.value) }} />
-          <input type='text' className="add-ressource-details__content__left__subtitle" style={{ marginBottom: '20px' }} placeholder="Total de pose" onChange={(e) => { setImageTotalExposure(e.target.value) }} />
-          <input type="file" accept='image/*' onChange={(e) => { setImageFile(e.target.files![0]) }} />
-          <button style={{ color: 'black', marginTop: '20px', marginBottom: '20px' }} onClick={() => addNewImage()}>Ajouter l'image</button>
-        </div>
-        <div className="add-ressource-details__content__right" style={{ textAlign: 'center' }}>
-          {imageFile && <img style={{ maxWidth: '90%' }} src={URL.createObjectURL(imageFile)} alt='' />}
-        </div> */}
       </div>
     </div>
   )
