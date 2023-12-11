246d9a256995474ed7e4f01b012aafa6216e30b4
Enzo Avagliano <eavagliano@laprovence.com>
2023-12-11 20:00:34 +0100

    Cleared old gallery files no longer necessary

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,8 @@
 import CustomImageDisplay from '../components/CustomImageDisplay'
 import { useGallery } from '../contexts/GalleryContext'
-import { images } from '../scripts/helpers/gallery'
-import '../styles/pages/gallery.scss'
 import { FiChevronLeft } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import '../styles/pages/gallery.scss'
 
 export default function Gallery() {
 
@@ -14,6 +13,24 @@ export default function Gallery() {
       <div className="gallery">
         <h1 className="h1 title"><Link to={"/"}><FiChevronLeft style={{ verticalAlign: 'middle' }} /></Link>Galerie photos</h1>
         <p>Retrouvez une sélection de mes photos favorites, lors de mes différentes sessions d'observation.</p>
+        {/* <div className="filters">
+          <div className="filter">
+            <input type="radio" id="all" name="filter" value="all" defaultChecked />
+            <label htmlFor="all">Toutes</label>
+          </div>
+          <div className="filter">
+            <input type="radio" id="moon" name="filter" value="moon" />
+            <label htmlFor="moon">Lune</label>
+          </div>
+          <div className="filter">
+            <input type="radio" id="planets" name="filter" value="planets" />
+            <label htmlFor="planets">Planètes</label>
+          </div>
+          <div className="filter">
+            <input type="radio" id="deep-sky" name="filter" value="deep-sky" />
+            <label htmlFor="deep-sky">Objets du ciel profond</label>
+          </div>
+        </div> */}
         <CustomImageDisplay
           images={pictures}
         />
