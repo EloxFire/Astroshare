926f0dcc38a657cd70460e14a1735c3b9d74a521
Enzo Avagliano <eavagliano@laprovence.com>
2024-03-19 17:20:31 +0100

    WIP : Observerly working revert to 0.31

--- src/components/planner/panels/astronomy/ObjectItem.tsx
+++ src/components/planner/panels/astronomy/ObjectItem.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { DeepSkyObject } from '../../../../scripts/types/DeepSkyObject'
 import { useAstro } from '../../../../contexts/AstroAppContext';
 import { getObjectName } from '../../../../scripts/helpers/astronomy/getObjectName';
 import { AstroObjectTypes } from '../../../../scripts/enums/AstroObjectTypes';
 import ObjectProperty from './ObjectProperty';
 import '../../../../styles/components/planner/panels/astronomy/objectItem.scss'
+import { convertEquatorialToHorizontal } from '@observerly/astrometry';
+import { useWeather } from '../../../../contexts/WeatherAppContext';
+import dayjs from 'dayjs';
+import { timeRaToRadians } from '../../../../scripts/helpers/astronomy/timeRaToRadians';
 
 interface ObjectItemProps {
   object: DeepSkyObject
@@ -14,6 +18,18 @@ interface ObjectItemProps {
 export default function ObjectItem({ object, currentlyVisible }: ObjectItemProps) {
   
   const { changeSelectedObject, selectedObject, currentCatalog } = useAstro();
+  const { city } = useWeather()
+
+  useEffect(() => {
+    
+    // const objectCoords = { ra: timeRaToRadians(object.ra), dec: object.dec }
+    
+    // if (city) {
+    //   const {alt, az} = convertEquatorialToHorizontal(new Date(), {city.lat, city.lng}, {ra: timeRaToRadians(object.ra), dec: object.dec})
+    // }
+    
+    // const observer = { city, city }
+  }, [])
 
   return (
     <button className={`astro-object-item ${currentlyVisible && 'well-visible'} ${selectedObject === object && 'active-object'}`} style={{gap: selectedObject ? '50px' : '100px'}} key={`object-${getObjectName(object, currentCatalog, false)}`} onClick={() => changeSelectedObject(object)}>
--- src/components/planner/panels/astronomy/ObjectsList.tsx
+++ src/components/planner/panels/astronomy/ObjectsList.tsx
@@ -1,45 +1,56 @@
-import React, { useState } from 'react'
-import '../../../../styles/components/planner/panels/astronomy/objectsList.scss'
-import { useAstro } from '../../../../contexts/AstroAppContext';
-import ObjectItem from './ObjectItem';
-import { DeepSkyObject } from '../../../../scripts/types/DeepSkyObject';
-import dayjs from 'dayjs';
-import { useWeather } from '../../../../contexts/WeatherAppContext';
-import { calculateCelestialPosition } from '../../../../scripts/helpers/astronomy/calculateCelestialPosition';
-import { getObjectName } from '../../../../scripts/helpers/astronomy/getObjectName';
+import React from "react";
+import { useAstro } from "../../../../contexts/AstroAppContext";
+import { DeepSkyObject } from "../../../../scripts/types/DeepSkyObject";
+import ObjectItem from "./ObjectItem";
+import "../../../../styles/components/planner/panels/astronomy/objectsList.scss";
 
 export default function ObjectsList() {
-
-  const { currentList, currentCatalog, searchObject, currentZenith } = useAstro();
-  const { city } = useWeather();
+  const { currentList, currentCatalog, searchObject } = useAstro();
 
   const handleSearch = (value: string) => {
     searchObject(value);
-    
-  }
+  };
 
   return (
     <div className="astro-objects-list">
-      <p className="title" style={{display: 'flex', justifyContent: 'space-between'}}>
-        Objets : {currentCatalog === 'all' ? 'Tous' : currentCatalog === 'm' ? 'Messier' : currentCatalog === 'ngc' ? 'NGC' : 'IC'}
-        <input className="search-dso" type="text" onChange={(e) => handleSearch(e.target.value)} placeholder="Rechercher un objet..." />
+      <p
+        className="title"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        Objets :{" "}
+        {currentCatalog === "all"
+          ? "Tous"
+          : currentCatalog === "m"
+          ? "Messier"
+          : currentCatalog === "ngc"
+          ? "NGC"
+          : "IC"}
+        <input
+          className="search-dso"
+          type="text"
+          onChange={(e) => handleSearch(e.target.value)}
+          placeholder="Rechercher un objet..."
+        />
       </p>
       <div className="list">
-        {
-          currentList && currentList !== undefined ?
-            currentList.length > 0 ?
-              currentList.map((object: DeepSkyObject, index: number) => {
-                const celestialPosition = calculateCelestialPosition({coords: {lat: city.lat, long: city.lng}, dateTime: dayjs(), celestialObject: {RA: object.ra, Dec: object.dec, name: getObjectName(object, currentCatalog, false)}});
-                const IOV = celestialPosition.altitude > 0 && celestialPosition.altitude < 90;
-                console.log(celestialPosition);
-                
-                // const IOV = isObjectVisible(currentZenith.ra, formatAstroCoordinates(object.ra), formatAstroCoordinates(object.dec));
-                return <ObjectItem key={`${object.name}-${index}`} object={object} currentlyVisible={IOV} />
-              })
-              : <p>Aucun objet trouvé...</p>
-            : <p>Erreur de chargement...</p>
-        }
+        {currentList && currentList !== undefined ? (
+          currentList.length > 0 ? (
+            currentList.map((object: DeepSkyObject, index: number) => {
+              return (
+                <ObjectItem
+                  key={`${object.name}-${index}`}
+                  object={object}
+                  currentlyVisible={false}
+                />
+              );
+            })
+          ) : (
+            <p>Aucun objet trouvé...</p>
+          )
+        ) : (
+          <p>Erreur de chargement...</p>
+        )}
       </div>
     </div>
-  )
+  );
 }
