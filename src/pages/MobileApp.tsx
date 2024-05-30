16f086afda3cc048712f4c33b8206f59bdcafe27
Enzo Avagliano <eavagliano@laprovence.com>
2024-05-31 01:13:28 +0200

    WIP with framer.. it's quite hard with code

--- src/pages/MobileApp.tsx
+++ src/pages/MobileApp.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import '../styles/pages/mobileApp.scss'
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function MobileApp() {
 
+  const variants = {
+    home: { opacity: 0 },
+    search: { opacity: 1, transition: { duration: .3 } },
+    details: { opacity: 1, transition: { duration: .3 } },
+    weather: { opacity: 1, transition: { duration: .3 } },
+    solarWeather: { opacity: 1, transition: { duration: .3 } },
+  }
+
+  const { scrollY } = useScroll();
+  const mainImageScale = useTransform(scrollY, [300, 700], [1, 0.5]);
+  const imageSwitch = useTransform(scrollY, [0, 300, 700, 1000, 1300], ['home', 'search', 'details', 'weather', 'solarWeather']);
+
   return (
     <div className="mobile-app">
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 3,
-        }}
-      >
-        <h1 className="title">L'application Astroshare</h1>
-      </motion.span>
-      <motion.span
+      <motion.h1 className='title' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{duration: 3}}>L'application Astroshare</motion.h1>
+      <motion.h2 className="subtitle" initial={{ opacity: 0 }} animate={{ opacity: .5 }} transition={{duration: 2, delay: .5,}}>
+        &#47;&#47; Votre nouveau compagnon d'astronomie
+      </motion.h2>
+      <motion.div
+        style={{display: 'flex', justifyContent: 'center'}}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{
@@ -23,29 +31,49 @@ export default function MobileApp() {
           delay: .5,
         }}
       >
-        <h2 className="subtitle">// Votre nouveau compagnon d'astronomie</h2>
-      </motion.span>
-      <motion.span
-        style={{display: 'flex', justifyContent: 'center'}}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 2,
-          delay: 1,
-        }}
-      >
-        <img style={{maxWidth: '200px', marginBottom: '15vh', marginTop: "2vh"}} src="/images/icons/google-play-badge.png" alt="" />
-      </motion.span>
+        <img style={{maxWidth: '200px', marginBottom: '15vh', marginTop: '2vh'}} src="/images/icons/google-play-badge.png" alt=""/>
+      </motion.div>
       <motion.div
         className='main-image-container'
+        style={{scale: mainImageScale}}
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        // animate={imageSwitch}
         transition={{
           duration: 3,
           delay: 1,
         }}
+        variants={variants}
       >
-        <img className="main-image" src="/images/promo/app/home.min.png" alt="" />
+        <motion.img
+          className='main-image'
+          src="/images/promo/app/home.png"
+          alt=""
+          variants={variants}
+        />
+        <motion.img
+          className='main-image'
+          src="/images/promo/app/search.png"
+          alt=""
+          variants={variants}
+        />
+        <motion.img
+          className='main-image'
+          src="/images/promo/app/details.png"
+          alt=""
+          variants={variants}
+        />
+        <motion.img
+          className='main-image'
+          src="/images/promo/app/weather.png"
+          alt=""
+          variants={variants}
+        />
+        <motion.img
+          className='main-image'
+          src="/images/promo/app/solarWeather.png"
+          alt=""
+          variants={variants}
+        />
       </motion.div>
     </div>
   )
