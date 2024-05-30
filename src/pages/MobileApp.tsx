10334eeb871fd7b7629fd0fa664818947dbe7644
Enzo Avagliano <eavagliano@laprovence.com>
2024-05-31 00:10:35 +0200

    New app landing - better

--- src/pages/MobileApp.tsx
+++ src/pages/MobileApp.tsx
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import '../styles/pages/mobileApp.scss'
-import { mockupAnimationImageSequence } from '../scripts/helpers/constants'
+import { motion } from "framer-motion";
 
 export default function MobileApp() {
 
-  const [currentImage, setCurrentImage] = useState<string>(mockupAnimationImageSequence[0])
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-
-  const handleScroll = () => {
-    // si le scroll augmente de 10px alors on affiche l'image suivante
-    // si le scroll diminue de 10px alors on affiche l'image précédente
-    // J'ai 70 images au total, mappe les images pour les afficher en fonction du scroll
-    // la taille de chaque image est de 80vh et la taille du conteuneur est de 400vh
-
-    const scroll = window.scrollY
-    const imageHeight = 80
-    const containerHeight = 400
-    const totalImages = 70
-    const imagesPerContainer = 5
-    const container = Math.floor(scroll / containerHeight)
-    const imageIndex = Math.floor((scroll % containerHeight) / imageHeight)
-    const image = container * imagesPerContainer + imageIndex
-    setCurrentImage(mockupAnimationImageSequence[image])
-
-  }
-
   return (
     <div className="mobile-app">
-      <img src="/images/promo/app/sequence/out1.png" style={{height: '80vh', position: 'sticky'}} alt="" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 3,
+        }}
+      >
+        <h1 className="title">L'application Astroshare</h1>
+      </motion.span>
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 2,
+          delay: .5,
+        }}
+      >
+        <h2 className="subtitle">// Votre nouveau compagnon d'astronomie</h2>
+      </motion.span>
+      <motion.span
+        style={{display: 'flex', justifyContent: 'center'}}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 2,
+          delay: 1,
+        }}
+      >
+        <img style={{maxWidth: '200px', marginBottom: '15vh', marginTop: "2vh"}} src="/images/icons/google-play-badge.png" alt="" />
+      </motion.span>
+      <motion.div
+        className='main-image-container'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 3,
+          delay: 1,
+        }}
+      >
+        <img className="main-image" src="/images/promo/app/home.min.png" alt="" />
+      </motion.div>
     </div>
   )
 }
